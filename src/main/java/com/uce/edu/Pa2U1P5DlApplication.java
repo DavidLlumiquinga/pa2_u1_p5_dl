9b13a0c4f52c009d1337bbd3476990e247e77fbd
unknown <mdllumiquingat@uce.edu.ec>
2023-11-07 19:18:43 -0500

    mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5DlApplication.java
@@ -0,0 +1,23 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+@SpringBootApplication
+public class Pa2U1P5DlApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5DlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Soy David Llumiquinga");
+	}
+
+}

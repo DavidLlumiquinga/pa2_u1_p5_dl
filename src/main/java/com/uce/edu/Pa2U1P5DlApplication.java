cbf501dbc64226b3923e7753de2152f65e922f97
DavidLlumiquinga <mdllumiquingat@uce.edu.ec>
2023-11-28 19:46:58 -0500

    Taller12

--- src/main/java/com/uce/edu/Pa2U1P5DlApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5DlApplication.java
@@ -17,11 +17,26 @@ import com.uce.edu.transferencia.service.ITransferenciaService;
 @SpringBootApplication
 public class Pa2U1P5DlApplication implements CommandLineRunner {
 
+	//DI por atributo
 	@Autowired
 	private ITransferenciaService iTransferenciaService;
-
+	
 	@Autowired
 	private ICuentaBancariaService bancariaService;
+
+	/*
+	 * DI for constructor
+	 * @Autowired public Pa2U1P5DlApplication(ITransferenciaService iTransServi) {
+	 * this.iTransferenciaService=iTransServi; }
+	 */
+
+	/*
+	 * DI for metodo set()
+	@Autowired
+	public void setiTransferenciaService(ITransferenciaService iTransferenciaService) {
+		this.iTransferenciaService = iTransferenciaService;
+	}
+	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5DlApplication.class, args);
@@ -36,32 +51,27 @@ public class Pa2U1P5DlApplication implements CommandLineRunner {
 		ctaOrigen.setNumero("12345");
 		ctaOrigen.setSaldo(new BigDecimal(100));
 		this.bancariaService.guardar(ctaOrigen);
-		
-		
 
 		CuentaBancaria ctaDestino = new CuentaBancaria();
 		ctaDestino.setCedulaPropietario("1725456895");
 		ctaDestino.setNumero("654321");
 		ctaDestino.setSaldo(new BigDecimal(200));
 		this.bancariaService.guardar(ctaDestino);
-		
+
 		this.bancariaService.depositar("12345", new BigDecimal(200));
 		System.out.println(ctaOrigen);
-/*
-		this.iTransferenciaService.realizar("12345", "654321", new BigDecimal(50));
 
-		
+		this.iTransferenciaService.realizar("12345", "654321", new BigDecimal(50));
 
 		this.iTransferenciaService.realizar("12345", "654321", new BigDecimal(30));
 		this.iTransferenciaService.realizar("654321", "12345", new BigDecimal(10));
-		
+
 		// Construir un estado de cuenta de todas las transferencia
 
 		List<Transferencia> lista = new ArrayList<>();
 		int indice = 0;
 		for (Transferencia trans : lista) {
-		
-			
+
 			indice++;
 			System.out.println(indice + " : " + trans);
 		}
@@ -69,8 +79,7 @@ public class Pa2U1P5DlApplication implements CommandLineRunner {
 		System.out.println(ctaOrigen1);
 		CuentaBancaria ctaDestino1 = this.bancariaService.buscar("654321");
 		System.out.println(ctaDestino1);
-*/
-		
+
 	}
 
 }
